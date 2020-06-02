11ae6f8284b131e4b6186f8da0ce7e871dd09c83
Nauman <naumanahmed19@gmail.com>
2020-06-02 11:08:21 +0200

    enh(Version): Changed Version

--- rekord-theme-essential.php
+++ rekord-theme-essential.php
@@ -3,7 +3,7 @@
  * Plugin Name: Rekord Theme Essential
  * Description: A must have plugin to enhance Rekord theme functionality.
  * Plugin URI:  https://xvelopers.com/
- * Version:     1.3.6
+ * Version:     1.3.7
  * Author:      Nomi
  * Author URI:  https://xvelopers.com/
  * Text Domain: rekord
