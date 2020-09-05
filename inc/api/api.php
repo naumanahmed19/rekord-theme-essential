f7cf2b50a13cd7cfae9b45fbd31da0e0a832d775
Nauman <naumanahmed19@gmail.com>
2020-09-05 14:23:43 +0200

    enh: api added

---
+++ inc/api/api.php
@@ -0,0 +1,147 @@
+<?php
+/**
+ * Plugin Name: Rekord API
+ * Plugin URI: 
+ * Description: API for Rekord WordPress Theme
+ * Version: 1.0
+ * Author: xvelopers
+ * Author URI: http://xvelopers.com
+ */
+
+
+function rekord_api_get_albums() {
+
+	$posts = rekord_api_get_posts('album');
+	$data = [];
+		$i = 0;
+	if(!empty($posts)){
+		foreach($posts as $post) {
+			$data[$i]['id'] = $post->ID;
+			$data[$i]['title'] = $post->post_title;
+			$data[$i]['content'] = $post->post_content;
+			$data[$i]['slug'] = $post->post_name;
+			$data[$i]['featured_image']['thumbnail'] = get_the_post_thumbnail_url($post->ID, 'thumbnail');
+			$data[$i]['featured_image']['medium'] = get_the_post_thumbnail_url($post->ID, 'medium');
+			$data[$i]['featured_image']['large'] = get_the_post_thumbnail_url($post->ID, 'large');
+			$data[$i]['tracks'] = rekord_album_tracks($id);
+			
+			$i++;
+		}
+	}
+	return [
+		'data' => $data,
+		'status' => 200
+	];
+}
+
+function rekord_album_tracks($id){
+	$args = rekord_relation_args('track' , 'track_albums',-1, $id);
+	$posts = get_posts($args); //builtin method
+	return rekord_data_tracks($posts);
+}
+
+
+function rekord_api_get_tracks() {
+	$posts = rekord_api_get_posts('track');
+	$data = rekord_data_tracks($posts);
+	return [
+		'data' => $data,
+		'status' => 200
+	];
+}
+
+
+
+
+function rekord_data_tracks($posts){
+
+	$data = [];
+	$i = 0;
+
+	if(!empty($posts)){
+		foreach($posts as $post) {
+
+
+			$url =  rekord_get_field('track_upload',$post->ID)['url'];
+			if(rekord_get_field('track',$post->ID) != 'upload'):
+				$url = rekord_get_field('track_url',$post->ID)  ;
+			  endif;   
+		
+			$data[$i]['id'] = $post->ID;
+			$data[$i]['title'] = $post->post_title;
+			$data[$i]['content'] = $post->post_content;
+			$data[$i]['slug'] = $post->post_name;
+			$data[$i]['type'] =  rekord_get_field('track',$post->ID);
+			$data[$i]['stream_type'] =  rekord_get_field('stream_type',$post->ID);
+			$data[$i]['url'] = $url ;
+			$data[$i]['featured_image']['thumbnail'] = get_the_post_thumbnail_url($post->ID, 'thumbnail');
+			$data[$i]['featured_image']['medium'] = get_the_post_thumbnail_url($post->ID, 'medium');
+			$data[$i]['featured_image']['large'] = get_the_post_thumbnail_url($post->ID, 'large');
+			$data[$i]['time'] = rekord_get_field('track_time', $post->ID);
+			$data[$i]['artist'] =  rekord_get_field('track_artists',$post->ID);
+	
+
+//$data[$i]['tracks'] = rekord_relation_args('track' , 'track_albums',-1);
+			$i++;
+		}
+	}
+	return $data;
+}
+
+
+
+
+function rekord_api_get_posts($post_type){
+	$paged = $_GET['paged'] ?$_GET['paged']: 1;
+	$postsPerPage = !empty($_GET['numberposts'] )? $_GET['numberposts'] : 2;
+	$postOffset = $paged * $postsPerPage;
+	$args = array(
+		'posts_per_page'  => $postsPerPage,
+		//'category_name'   => $btmetanm,
+		'offset'          => $postOffset,
+		'post_type'       => $post_type
+	);
+
+	return  get_posts($args);
+}
+
+
+
+
+
+function wl_post( $slug ) {
+	$args = [
+		'name' => $slug['slug'],
+		'post_type' => 'post'
+	];
+
+	$post = get_posts($args);
+
+
+	$data['id'] = $post[0]->ID;
+	$data['title'] = $post[0]->post_title;
+	$data['content'] = $post[0]->post_content;
+	$data['slug'] = $post[0]->post_name;
+	$data['featured_image']['thumbnail'] = get_the_post_thumbnail_url($post[0]->ID, 'thumbnail');
+	$data['featured_image']['medium'] = get_the_post_thumbnail_url($post[0]->ID, 'medium');
+	$data['featured_image']['large'] = get_the_post_thumbnail_url($post[0]->ID, 'large');
+
+	return $data;
+}
+
+add_action('rest_api_init', function() {
+
+	$routes = ['posts','albums','tracks'];
+	foreach($routes as $route){
+		register_rest_route('wl/v1', $route, [
+			'methods' => 'GET',
+			'callback' => 'rekord_api_get_'.$route,
+		]);
+	}
+
+
+	register_rest_route( 'wl/v1', 'posts/(?P<slug>[a-zA-Z0-9-]+)', array(
+		'methods' => 'GET',
+		'callback' => 'wl_post',
+	) );
+});
