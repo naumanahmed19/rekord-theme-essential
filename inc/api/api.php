27dbd1571d0cf832a51686e3b778d7153f77cbf6
Nauman <naumanahmed19@gmail.com>
2020-09-18 19:26:10 +0200

    enh(api): expolre api

--- inc/api/api.php
+++ inc/api/api.php
@@ -12,8 +12,19 @@
 function rekord_api_get_albums() {
 
 	$posts = rekord_api_get_posts('album');
+	$data =  rekord_api_attach_album_fields($posts);
+
+	
+	return [
+		'data' => $data,
+		'status' => 200
+	];
+}
+
+
+function rekord_api_attach_album_fields($posts){
 	$data = [];
-		$i = 0;
+	$i = 0;
 	if(!empty($posts)){
 		foreach($posts as $post) {
 			$data[$i]['id'] = $post->ID;
@@ -29,16 +40,16 @@ function rekord_api_get_albums() {
 			$i++;
 		}
 	}
-	return [
-		'data' => $data,
-		'status' => 200
-	];
+	return $data;
 }
+
+
+
 
 function rekord_album_tracks($id){
 	$args = rekord_relation_args('track' , 'track_albums',-1, $id);
 	$posts = get_posts($args); //builtin method
-	return rekord_data_tracks($posts);
+	return rekord_api_attach_track_fields($posts);
 }
 
 
@@ -46,7 +57,7 @@ function rekord_api_get_tracks() {
 
 
 	$posts = rekord_api_get_posts('track');
-	$data = rekord_data_tracks($posts);
+	$data =  rekord_api_attach_track_fields($posts);
 	return [
 		'data' => $data,
 		'status' => 200
@@ -57,7 +68,7 @@ function rekord_api_get_tracks() {
 
 
 
-function rekord_data_tracks($posts){
+function rekord_api_attach_track_fields($posts){
 
 	$data = [];
 	$i = 0;
@@ -126,6 +137,52 @@ function rekord_api_get_posts($post_type){
 }
 
 
+function rekord_api_get_explore($post_type){
+
+	$data = [];
+	$i = 0;
+
+	$sections =  rekord_get_field('explore_screen', 'option');
+
+	foreach($sections as $section){
+		$data[$i]['title'] = $section['title'];
+		$data[$i]['type'] = $section['select_post_type'];
+		$data[$i]['number_of_post'] = $section['number_of_post'];
+
+
+		$args = array(
+			'posts_per_page'  => $section['number_of_post'],
+			//'category_name'   => $btmetanm,
+			'offset'          => $postOffset,
+			'post_type'       =>  $section['select_post_type']
+		);
+
+		
+		if($section['select_post_type'] == 'album'){
+			$posts = get_posts($args);
+			$data[$i]['posts'] = rekord_api_attach_album_fields($posts );
+		}
+		
+		if($section['select_post_type'] == 'track'){
+
+			$posts = rekord_api_get_posts('track');
+		
+			$data[$i]['posts'] =rekord_api_attach_track_fields($posts);
+		}
+
+		//$data['sections'][$i] = $section['select_post_type'];	
+
+		$i++;
+	}
+
+	
+	return [
+		'data' => $data,
+		'status' => 200
+	];
+}
+
+
 
 
 
@@ -153,7 +210,7 @@ function wl_post( $slug ) {
 add_action('rest_api_init', function() {
 
 	
-	$routes = ['posts','albums','tracks','taxonomy'];
+	$routes = ['explore','posts','albums','tracks','taxonomy'];
 	foreach($routes as $route){
 		register_rest_route('wl/v1', $route, [
 			'methods' => 'GET',
@@ -167,3 +224,145 @@ add_action('rest_api_init', function() {
 		'callback' => 'wl_post',
 	) );
 });
+
+
+
+
+if( function_exists('acf_add_local_field_group') ):
+
+	acf_add_local_field_group(array(
+		'key' => 'group_5f64bff58c3d2',
+		'title' => 'Rekord Mobile API',
+		'fields' => array(
+			array(
+				'key' => 'field_5f64c23da4525',
+				'label' => 'Explore Screen',
+				'name' => 'explore_screen',
+				'type' => 'repeater',
+				'instructions' => 'Add explore screen sections',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'collapsed' => '',
+				'min' => 0,
+				'max' => 0,
+				'layout' => 'block',
+				'button_label' => '',
+				'sub_fields' => array(
+					array(
+						'key' => 'field_5f64c388a4526',
+						'label' => 'Title',
+						'name' => 'title',
+						'type' => 'text',
+						'instructions' => 'Add section title',
+						'required' => 0,
+						'conditional_logic' => 0,
+						'wrapper' => array(
+							'width' => '',
+							'class' => '',
+							'id' => '',
+						),
+						'default_value' => '',
+						'placeholder' => '',
+						'prepend' => '',
+						'append' => '',
+						'maxlength' => '',
+					),
+					array(
+						'key' => 'field_5f64c5cad50c7',
+						'label' => 'Select Post Type',
+						'name' => 'select_post_type',
+						'type' => 'select',
+						'instructions' => '',
+						'required' => 0,
+						'conditional_logic' => 0,
+						'wrapper' => array(
+							'width' => '',
+							'class' => '',
+							'id' => '',
+						),
+						'choices' => array(
+							'album' => 'album',
+							'track' => 'track',
+							'event' => 'event',
+							'podcast' => 'podcast',
+						),
+						'default_value' => array(
+						),
+						'allow_null' => 0,
+						'multiple' => 0,
+						'ui' => 0,
+						'return_format' => 'value',
+						'ajax' => 0,
+						'placeholder' => '',
+					),
+					array(
+						'key' => 'field_5f64c657d50c8',
+						'label' => 'Number of Post',
+						'name' => 'number_of_post',
+						'type' => 'number',
+						'instructions' => '',
+						'required' => 0,
+						'conditional_logic' => 0,
+						'wrapper' => array(
+							'width' => '',
+							'class' => '',
+							'id' => '',
+						),
+						'default_value' => 5,
+						'placeholder' => '',
+						'prepend' => '',
+						'append' => '',
+						'min' => '',
+						'max' => '',
+						'step' => '',
+					),
+					array(
+						'key' => 'field_5f64c70198043',
+						'label' => 'Select A category',
+						'name' => 'select_a_category',
+						'type' => 'taxonomy',
+						'instructions' => '',
+						'required' => 0,
+						'conditional_logic' => 0,
+						'wrapper' => array(
+							'width' => '',
+							'class' => '',
+							'id' => '',
+						),
+						'taxonomy' => 'category',
+						'field_type' => 'select',
+						'allow_null' => 0,
+						'add_term' => 0,
+						'save_terms' => 0,
+						'load_terms' => 0,
+						'return_format' => 'id',
+						'multiple' => 0,
+					),
+				),
+			),
+		),
+		'location' => array(
+			array(
+				array(
+					'param' => 'options_page',
+					'operator' => '==',
+					'value' => 'podcast-rss-settings',
+				),
+			),
+		),
+		'menu_order' => 0,
+		'position' => 'normal',
+		'style' => 'default',
+		'label_placement' => 'top',
+		'instruction_placement' => 'label',
+		'hide_on_screen' => '',
+		'active' => true,
+		'description' => '',
+	));
+	
+	endif;
